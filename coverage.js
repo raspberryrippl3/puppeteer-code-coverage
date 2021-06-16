dccf2ae544442877fe1fb26225314eddf464a5b2
raspberryrippl3 <46265707+raspberryrippl3@users.noreply.github.com>
2021-06-16 17:01:40 +0100

    Update coverage.js

--- coverage.js
+++ coverage.js
@@ -1,48 +1,110 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const util = require('util');
 const fs = require("fs");
-
-const urls = ['http://steam.test/index.html','http://steam.test/index2.html'];
-
-// the configs you want to test
-const configs = [
-  {url: 'http://steam.test/index.html'},
-  {url: 'http://steam.test/index2.html',},
+const urls = [
+  'http://steam.test',
+  'http://steam.test/contact-us/',
+  'http://steam.test/blog/',
+  'http://steam.test/404',
+  'http://steam.test/?s=test',
 ];
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
 
 (async () => {
 
-const coverage = async ({ url }) => {
-
-	let browser = await puppeteer.launch();
-	let page = await browser.newPage();
-
+	const browser = await puppeteer.launch({ headless: false, ignoreHTTPSErrors: true, args: ['--window-size=1920,1080'] });
+	const page = await browser.newPage();
 	await page.coverage.startCSSCoverage({ resetOnNavigation: false });
-	await page.goto(url);
-	await page.reload();
-	
-	let cssCoverage = await page.coverage.stopCSSCoverage();
+
+	for (let i = 0; i < urls.length; i++) {
+		const url = urls[i];
+		try {
+			await page.setViewport({
+				width: 575,
+				height: 600,
+				deviceScaleFactor: 2,
+			});
+			await page.goto(url);
+			await page.click( '.svg-trigger' );
+			await page.waitFor(1000);
+			await page.click( '.sub-menu-trigger' );
+			await page.waitFor(2000);
+			
+			await page.setViewport({
+				width: 767,
+				height: 600,
+				deviceScaleFactor: 2,
+			});
+			await page.reload();
+
+			await page.waitFor(3000);
+
+			await page.setViewport({
+				width: 991,
+				height: 600,
+				deviceScaleFactor: 2,
+			});
+			await page.reload();
+
+			await page.waitFor(3000);
+
+			await page.setViewport({
+				width: 1199,
+				height: 800,
+				deviceScaleFactor: 1,
+			});
+			await page.reload();
+
+			await page.waitFor(3000);
+
+			await page.setViewport({
+				width: 1399,
+				height: 1024,
+				deviceScaleFactor: 1,
+			});
+			await page.reload();
+
+			await page.waitFor(3000);
+
+			await page.setViewport({
+				width: 1920,
+				height: 1080,
+				deviceScaleFactor: 1,
+			});
+			await page.reload();
+			await autoScroll(page);
+			await page.waitFor(3000);
+		}
+	catch (err) {}
+	}
+  
+	cssCoverage = await page.coverage.stopCSSCoverage();
 
 	await browser.close();
-	
-	return cssCoverage;
 
-};
-
-const results = [];
-
-for(const config of configs) {
-	const result = await coverage(config);
-	results.push(result);
-}
-
-//console.log( JSON.stringify(results, null, 2) );
-
-fs.writeFile("./exported_css.json", JSON.stringify(results, null, 2), function(err) {
-	if(err) {
-		return console.log(err);
-	}
+	fs.writeFile("./exported_css.json", JSON.stringify(cssCoverage, null, 2), function(err) {
+		if(err) {
+			return console.log(err);
+		}
 	console.log("The file was saved!");
-}); 
-
-})()
+	});   
+  
+})();
